a328ad9a5fcb88cbaca08cb9e5d3dcf3a390ced9
Misha Samoylov <mikhailsamoylov95@gmail.com>
2022-05-15 11:15:12 +0700

    Added partially parser CHUNK_FRAMELIST

--- dff_loader.h
+++ dff_loader.h
@@ -17,12 +17,27 @@ struct clump_data {
 	uint32_t num_cameras;
 };
 
+struct framelist_data {
+	uint32_t num_frames;
+};
+
+struct framelist_frame {
+	float rotation_matrix[3 * 3];
+	float position[3];
+	int32_t parent_frame_id;
+	uint32_t matrix_creation_flag;
+};
+
 enum CHUNK_TYPE {
 	CHUNK_CLUMP = 0x10,
-	CHUNK_STRUCT = 0x1
+	CHUNK_STRUCT = 0x1,
+	CHUNK_FRAMELIST = 0xE
 };
 
 int dff_load(char *bytes);
+
+int read_header(char *bytes, int *offset, int check_chunk_type);
 int read_chunk_clump(char *bytes, int *offset);
+int read_chunk_framelist(char *bytes, int *offset);
 
 #endif
