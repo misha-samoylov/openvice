cbe5185126fd0d2b247a968ea92d69478927d249
Misha Samoylov <mikhailsamoylov95@gmail.com>
2023-05-14 20:19:17 +0700

    Fix texture repeating

--- GameModel.cpp
+++ GameModel.cpp
@@ -178,8 +178,9 @@ HRESULT GameModel::SetDataDDS(GameRender* pRender, uint8_t* source, size_t size,
 	sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
 	sampDesc.Filter = D3D11_FILTER_ANISOTROPIC; //
 	sampDesc.MaxAnisotropy = 16; //
-	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP; // D3D11_TEXTURE_ADDRESS_WRAP
+	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP; // Повторять текстуру
 	sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
 	sampDesc.MinLOD = 0;
 	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
